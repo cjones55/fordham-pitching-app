dcf828366f84c45acff8346ad944dc1d5abeece8
cjones55 <chrisjjones555@gmail.com>
2026-04-16 23:54:55 -0400

    Update app.py

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ try:
         <style>
             .top-left-logo {{
                 position: fixed;
-                top: 50px;
+                top: 57px;
                 left: 12px;
                 width: 110px;
                 z-index: 99999;
